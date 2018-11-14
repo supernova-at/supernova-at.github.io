e6edd99ea7907b3852651ca68681a531e714ff98
Andrew Terranova <aterrano@ip-10-235-41-149.ec2.internal>
2018-11-14 15:42:04 -0600

    PWA Updates Part 1

--- webpack-testing/entry.js
+++
@@ -1,3 +0,0 @@
-var content_module = require('./lib/content');
-
-document.write(content_module.theString);
--- webpack-testing/lib/content/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	theString : 'It works from content.js exports 2'
-};
